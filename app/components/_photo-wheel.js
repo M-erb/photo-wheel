b32983f7a0a5f110755710abfc02743967bf7ed5
Merb <michael.erbs@gmail.com>
2017-09-30 16:03:44 -0700

    added link and new styles

--- app/components/_photo-wheel.js
+++ app/components/_photo-wheel.js
@@ -11,13 +11,21 @@ Vue.component('photo-wheel', {
           </li>
         </ul>
         <div class="wh_inner">
-          <div class="wh_instructions" v-if="!featuredItem.filled">
-            <p>Click on an image to view it</p>
-          </div>
-          <transition name="fade">
-            <div class="wh_featuredItem" v-if="featuredItem.filled" :style="{backgroundImage: 'url(' + featuredItem.lgImgUrl + ')'}">
-              <!-- <img :src="featuredItem.lgImgUrl" :alt="featuredItem.name"> -->
-              <p>{{featuredItem.name}}</p>
+          <transition name="fadeBoth">
+            <div class="wh_instructions" v-if="featuredItem.fill === ''">
+              <p>Click on an image to view it</p>
+            </div>
+          </transition>
+          <transition name="fadeIn">
+            <div class="wh_featuredItem" v-if="featuredItem.fill === 'one'" :style="{backgroundImage: 'url(' + featuredItem.lgImgUrl + ')'}">
+            <p class="pro_title">{{featuredItem.name}}</p>
+            <a class="pro_link" :href="featuredItem.url">See More</a>
+            </div>
+          </transition>
+          <transition name="fadeIn">
+            <div class="wh_featuredItem" v-if="featuredItem.fill === 'two'" :style="{backgroundImage: 'url(' + featuredItem.lgImgUrl + ')'}">
+              <p class="pro_title">{{featuredItem.name}}</p>
+              <a class="pro_link" :href="featuredItem.url">See More</a>
             </div>
           </transition>
         </div>
@@ -31,15 +39,17 @@ Vue.component('photo-wheel', {
         miniImgUrl: '',
         lgImgUrl: '',
         name: '',
+        url: '',
         details: '',
         type: '',
-        filled: false
+        fill: ''
       },
       wheelImgs: [
         {
           miniImgUrl: 'content/img/200_christopher-ruel-221920.jpg',
           lgImgUrl: 'content/img/600_christopher-ruel-221920.jpg',
           name: 'Project One',
+          url: '#projectOne',
           details: 'Some details for project one',
           type: 'Mechanical',
           position: 0
@@ -47,6 +57,7 @@ Vue.component('photo-wheel', {
           miniImgUrl: 'content/img/200_elena-saharova-215516.jpg',
           lgImgUrl: 'content/img/600_elena-saharova-215516.jpg',
           name: 'Project Two',
+          url: '#projectTwo',
           details: 'Some details for project Two',
           type: 'Mechanical',
           position: 45
@@ -54,6 +65,7 @@ Vue.component('photo-wheel', {
           miniImgUrl: 'content/img/200_img3.jpg',
           lgImgUrl: 'content/img/600_img3.jpg',
           name: 'Project Three',
+          url: '#projectThree',
           details: 'Some details for project Three',
           type: 'Mechanical',
           position: 90
@@ -61,6 +73,7 @@ Vue.component('photo-wheel', {
           miniImgUrl: 'content/img/200_joel-filipe-226909.jpg',
           lgImgUrl: 'content/img/600_joel-filipe-226909.jpg',
           name: 'Project Four',
+          url: '#projectFour',
           details: 'Some details for project Four',
           type: 'Mechanical',
           position: 135
@@ -68,6 +81,7 @@ Vue.component('photo-wheel', {
           miniImgUrl: 'content/img/200_jonas-jacobsson-344603.jpg',
           lgImgUrl: 'content/img/600_jonas-jacobsson-344603.jpg',
           name: 'Project Five',
+          url: '#projectFive',
           details: 'Some details for project Five',
           type: 'Mechanical',
           position: 180
@@ -75,6 +89,7 @@ Vue.component('photo-wheel', {
           miniImgUrl: 'content/img/200_img6.jpg',
           lgImgUrl: 'content/img/600_img6.jpg',
           name: 'Project Six',
+          url: '#projectSix',
           details: 'Some details for project Six',
           type: 'Mechanical',
           position: 225
@@ -82,6 +97,7 @@ Vue.component('photo-wheel', {
           miniImgUrl: 'content/img/200_img7.jpg',
           lgImgUrl: 'content/img/600_img7.jpg',
           name: 'Project Seven',
+          url: '#projectSeven',
           details: 'Some details for project Seven',
           type: 'Mechanical',
           position: 270
@@ -89,6 +105,7 @@ Vue.component('photo-wheel', {
           miniImgUrl: 'content/img/200_img8.jpg',
           lgImgUrl: 'content/img/600_img8.jpg',
           name: 'Project Eight',
+          url: '#projectEight',
           details: 'Some details for project Eight',
           type: 'Mechanical',
           position: 315
@@ -96,13 +113,17 @@ Vue.component('photo-wheel', {
       ],
     }
   },
+  props: {},
   methods: {
     selectItem(item) {
       var vm = this
-      vm.featuredItem.filled =false
       vm.curPosition = item.position
       vm.featuredItem = Object.assign(vm.featuredItem, item)
-      vm.featuredItem.filled = true
+      if (vm.featuredItem.fill === '' || vm.featuredItem.fill === 'one') {
+        vm.featuredItem.fill = 'two'
+      } else if (vm.featuredItem.fill === 'two') {
+        vm.featuredItem.fill = 'one'
+      }
     }
   }
 })
